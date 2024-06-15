93e84163d73734efea80c97cc2c88c16dea19915
quartzyeti <Donnie373@gmail.com>
2024-06-14 21:37:51 -0700

    Fri Jun 14 21:37:38 PDT 2024

--- shell/mack-a-English_Removal.sh
+++ shell/mack-a-English_Removal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Rename the current install.sh to install_en.sh
-mv /root/install.sh /root/install_en.sh
+mv /etc/v2ray-agent/install.sh /etc/v2ray-agent/install_en.sh
 
 # Rename original_install.sh back to install.sh
-mv /root/original_install.sh /root/install.sh
+mv /etc/v2ray-agent/original_install.sh /etc/v2ray-agent/install.sh
 
 # Set permissions for the renamed install.sh
-chmod 700 /root/install.sh
+chmod 700 /etc/v2ray-agent/install.sh
