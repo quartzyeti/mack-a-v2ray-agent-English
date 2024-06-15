1c6da28bc422142f4e0f0d894374f254afb89a2c
quartzyeti <Donnie373@gmail.com>
2024-06-14 21:16:13 -0700

    Fri Jun 14 21:16:00 PDT 2024

--- shell/mack-a-English_Setup-2.sh
+++ shell/mack-a-English_Setup-2.sh
@@ -10,17 +10,17 @@ chmod 700 /root/install.sh
 /root/install.sh &
 
 # Give it some time to execute and bring up the menu (adjust the sleep duration as needed)
-sleep 3
+sleep 1
 
 # Find the process ID (PID) of the install.sh script and kill it
 pkill -f /root/install.sh
 
 # Rename the original install.sh to original_install.sh
-mv /root/install.sh /root/original_install.sh
+mv /etc/v2ray-agent/install.sh /etc/v2ray-agent/original_install.sh
 
 # Download the new install.sh and rename it
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/quartzyeti/mack-a-v2ray-agent-English/master/shell/install_en.sh"
-mv /root/install_en.sh /root/install.sh
+mv /root/install_en.sh /etc/v2ray-agent/install.sh
 
 # Set permissions for the new install.sh
-chmod 700 /root/install.sh
+chmod 700 /etc/v2ray-agent/install.sh && /etc/v2ray-agent/install.sh
